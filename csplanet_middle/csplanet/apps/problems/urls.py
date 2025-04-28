4de3009a745b2850eb70c754497d8a6a905c2a28
easy-min <100506494+easy-min@users.noreply.github.com>
2025-04-28 12:23:49 +0900

    update question register

--- csplanet_middle/csplanet/apps/problems/urls.py
+++ csplanet_middle/csplanet/apps/problems/urls.py
@@ -1,23 +1,33 @@
 # csplanet/apps/problems/urls.py
+
 from django.urls import path
+
+# 새로 만든 뷰 함수들을 import
+from .views.create_objective import (
+    create_objective,
+    detail_objective,
+    edit_objective,
+)
 from .views import (
     solve_problems,
     select_problem_type,
-    create_objective,
     create_subjective,
 )
 
 app_name = 'problems'
 
 urlpatterns = [
-    # 문제 목록
-    #path('',                   list_problems,         name='list'),
     # 객관식/주관식 출제 선택
     path('create/',            select_problem_type,   name='select_problem_type'),
-    # 문제 풀러가기 (앱 네임스페이스 안에서도 제공 가능)
-    path('solve/',             solve_problems,       name='solve_problems'),
-    # 객관식 문제 생성
+
+    # 문제 풀러가기
+    path('solve/',             solve_problems,        name='solve_problems'),
+
+    # — 객관식 문제 생성/상세/수정 —
     path('objective/create/',  create_objective,      name='create_objective'),
+    path('objective/<int:pk>/',     detail_objective,      name='detail_objective'),
+    path('objective/<int:pk>/edit/', edit_objective,        name='edit_objective'),
+
     # 주관식 문제 생성
     path('subjective/create/', create_subjective,     name='create_subjective'),
 ]
--- csplanet_middle/csplanet/apps/problems/views/create_objective.py
+++ csplanet_middle/csplanet/apps/problems/views/create_objective.py
@@ -1,7 +1,6 @@
-# csplanet/apps/problems/views/views.py
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_http_methods
+
 from ..forms.forms import ObjectiveQuestionForm, ObjectiveChoiceFormSet
 from ..models.objective_problem import ObjectiveProblem
 from ..models.base_problem import BaseProblem
@@ -10,36 +9,70 @@ from ..models.chapter import Chapter
 
 @require_http_methods(["GET", "POST"])
 def create_objective(request):
-    topics   = Topic.objects.all()
+    topics = Topic.objects.all()
     chapters = Chapter.objects.all()
 
     if request.method == "POST":
-        form    = ObjectiveQuestionForm(request.POST)
-        obj_q   = None
+        form = ObjectiveQuestionForm(request.POST)
+        obj_q = None
 
         if form.is_valid():
             obj_q = form.save(commit=False)
-            obj_q.creator       = request.user
-            obj_q.question_type = BaseProblem.MCQ    # ★ 꼭 MCQ로 세팅
+            obj_q.topic = obj_q.chapter.topic
+            obj_q.creator = request.user
+            obj_q.question_type = BaseProblem.MCQ
             obj_q.save()
 
-        # form 유효성 검사 후에도 반드시 instance 지정
         formset = ObjectiveChoiceFormSet(
             request.POST,
-            instance = obj_q or ObjectiveProblem()
+            instance=obj_q or ObjectiveProblem()
         )
 
         if form.is_valid() and formset.is_valid():
             formset.save()
-            return redirect('problems:solve_problems')
-
+            return redirect('problems:detail_objective', pk=obj_q.pk)
     else:
-        form    = ObjectiveQuestionForm()
+        form = ObjectiveQuestionForm()
         formset = ObjectiveChoiceFormSet(instance=ObjectiveProblem())
 
     return render(request, 'problems/create_objective.html', {
-        'form':     form,
-        'formset':  formset,
-        'topics':   topics,
+        'form': form,
+        'formset': formset,
+        'topics': topics,
         'chapters': chapters,
+    })
+
+@require_http_methods(["GET"])
+def detail_objective(request, pk):
+    problem = get_object_or_404(ObjectiveProblem, pk=pk)
+    return render(request, 'problems/objective_detail.html', {
+        'problem': problem
+    })
+
+@require_http_methods(["GET", "POST"])
+def edit_objective(request, pk):
+    problem = get_object_or_404(ObjectiveProblem, pk=pk)
+    topics = Topic.objects.all()
+    chapters = Chapter.objects.all()
+
+    if request.method == "POST":
+        form = ObjectiveQuestionForm(request.POST, instance=problem)
+        formset = ObjectiveChoiceFormSet(request.POST, instance=problem)
+
+        if form.is_valid() and formset.is_valid():
+            form.save()
+            formset.save()
+            return redirect('problems:detail_objective', pk=problem.pk)
+    else:
+        form = ObjectiveQuestionForm(instance=problem)
+        formset = ObjectiveChoiceFormSet(instance=problem)
+
+    return render(request, 'problems/create_objective.html', {
+        'form': form,
+        'formset': formset,
+        'topics': topics,
+        'chapters': chapters,
+        'problem':problem,
+        'edit': True,
+        
     })
